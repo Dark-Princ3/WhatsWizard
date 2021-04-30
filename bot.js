89188de9683ca7df9385609f32bc8e4a2d77c5da
Prince Mendiratta <prince.mendi@gmail.com>
2021-04-30 21:13:01 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -238,9 +238,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                 else {
                     await conn.sendMessage(conn.user.jid, '*WhatsAsena Working as Public! 🐺*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as public. To change it, use* _.setvar WORK_TYPE:private_\n\n*Thanks for using WhatsAsena 💌*', MessageType.text);
 
-                    await git.fetch();
-                    var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
-                    if (commits.total === 0) {
+                    var commits = 0;
+                    if (commits === 0) {
                         await conn.sendMessage(
                             conn.user.jid,
                             Lang.UPDATE, MessageType.text
@@ -330,9 +329,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
 
                     await conn.sendMessage(conn.user.jid, '*WhatsAsena Working as Private! 🐺*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as private. To change it, use* _.setvar WORK_TYPE:public_\n\n*Thanks for using WhatsAsena 💌*', MessageType.text);
 
-                    await git.fetch();
-                    var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
-                    if (commits.total === 0) {
+                    var commits = 0;
+                    if (commits === 0) {
                         await conn.sendMessage(
                             conn.user.jid,
                             Lang.UPDATE, MessageType.text
